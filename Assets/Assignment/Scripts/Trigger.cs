e76be8d05efe00566a8e421ebdaeb9c08594aee7
Shiro <fl34un@gmail.com>
2024-01-31 00:50:11 -0500

    Trigger script and setup

---
+++ Assets/Assignment/Scripts/Trigger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public GameObject arrow1;
+    public GameObject arrow2;
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        arrow1.SetActive(false);
+        arrow2.SetActive(true);
+    }
+    void Update()
+    {
+        
+    }
+}
